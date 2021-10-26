f137e5816e9c52a9e46aed5637d3a6914ae37c19
unknown <duckhang2210@gmail.com>
2021-10-26 16:54:51 -0500

    create template for components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div className='text-center p-10 mt-10 border-t dark:border-green-700 border-red-300'>
+            <h1>2021 Burning Company</h1>
+        </div>
+    )
+}

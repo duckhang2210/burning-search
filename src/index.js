5199a41b9c1cd12cba1950a69183fa126740d894
unknown <duckhang2210@gmail.com>
2021-10-26 17:41:46 -0500

    static search function

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDom from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import { ResultContextProvider } from './contexts/ResultContextProvider';
 import './global.css';
 
+require('dotenv').config();
+
 ReactDom.render(
-	<Router>
-		<App />
-	</Router>,
+	<ResultContextProvider>
+		<Router>
+			<App />
+		</Router>
+	</ResultContextProvider>,
 	document.getElementById('root')
 );

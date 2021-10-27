a3d617328c11b8c4db56832916eca539591ab530
unknown <duckhang2210@gmail.com>
2021-10-26 21:42:16 -0500

    add search function

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,8 +10,14 @@ export const Results = () => {
     const location = useLocation();
 
     useEffect(() => {
-        getResults('/search/q=javascript&num=40')
-    }, [])
+        if(searchTerm) {
+            if(location.pathname === '/videos') {
+                getResults(`/search/q=${searchTerm} videos`);
+            } else {
+                getResults(`${location.pathname}/q=${searchTerm}&num=40`)
+            }
+        }
+    }, [searchTerm, location.pathname])
 
     if(isLoading) return <Loading />
 
@@ -19,7 +25,7 @@ export const Results = () => {
         case '/search':
             return (
                 <div className="flex flex-wrap justify-between space-y-6 sm:px56">
-                    {results?.results?.map(({ link, title }, index) => (
+                    {results?.map(({ link, title }, index) => (
                         <div key={index} className='md:w-2/5 w-full'>
                             <a href={link} target='_blank' rel='noreferrer'>
                                 <p className="text-sm">
@@ -34,11 +40,48 @@ export const Results = () => {
                 </div>
             );
         case '/images':
-            return 'SEARCH';
+            return (
+                <div className="flex flex-wrap justify-center items-center">
+                    {results?.map(({ image, link: { href, title }}, index) => (
+                        <a href={href} key={index} target='_blank' rel='noreferrer' className="sm:p-3 p-5">
+                            <img src={image?.src} alt={title} loading='lazy' />
+                            <p className="w-36 break-words text-sm mt-2">
+                                {title}
+                            </p>
+                        </a>
+                    ))}
+                </div>
+            );
         case '/news':
-            return 'SEARCH';
+            return (
+                <div className="flex flex-wrap justify-between space-y-6 sm:px56 items-center">
+                    {results?.map(({ links, id, title, source }) => (
+                        <div key={id} className='md:w-2/5 w-full'>
+                            <a href={links?.[0].href} target='_blank' rel='noreferrer' className='hover:underline'>
+                                <p className="text-lg dark:text-blue-300 text-blue-700">
+                                    {title}
+                                </p>
+                            </a>
+                            <div className="flex gap-4">
+                                <a href={source?.href} target='_blank' rel='noreferrer'>
+                                    {source?.href}
+                                </a>
+                            </div>
+                            
+                        </div>
+                    ))}
+                </div>
+            );
         case '/videos':
-            return 'SEARCH';
+            return (
+                <div className="flex flex-wrap">
+                    {results?.map(( video, index ) => (
+                        <div key={index} className='p-2'>
+                            {video?.additional_links?.[0]?.href && <ReactPlayer url={video.additional_links?.[0].href} control width='355px' height='250px' /> }                            
+                        </div>
+                    ))}
+                </div>
+            );
         default:
             return 'ERROR'
     }
